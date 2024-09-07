d268a720018e26435b01afc77b078c568d67b53d
Ibis <ibistheoficial@gmail.com>
2024-09-07 12:33:53 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -16,5 +16,42 @@ let dados = [
         descricao : "Sua origem se perde no tempo, mas acredita-se que tenha surgido em civilizações antigas como oferenda aos deuses ou para celebrar ocasiões especiais.",
         link : "https://pt.wikipedia.org/wiki/Pão_doce",
         tags : "oferenda celebrasões antigo pao "
-    }
+    },
+    {
+        título : "Baguete",
+        descricao : "no século XVIII na França era muito consumido o pão escuro - chamado de “pão do povo” - pois o alimento feito com farinha branca era consumido principalmente pela nobreza/burguesia devido ao alto preço.[1] No século XIX a farinha branca popularizou-se e foi criado a baguete, com uma casca externa dourada e crocante e miolo interior macio.[1] E nesse período, a elite brasileira ao viajar à França impressionou-se com a baguete.[1] Ao retornar de viajar desejavam imitar este alimento nas padarias locais (que ainda faziam o pão escuro), predominantemente as controladas por portugueses.[1]",
+        link : "https://pt.wikipedia.org/wiki/Baguete",
+        tags : "longo  pao escuro do povo dourado casitinho "
+    },
+    {
+        título : "Pão australiano",
+        descricao : "o pão australiano, com sua cor escura e textura macia, na verdade, ganhou fama graças a uma rede de restaurantes americana. Essa iguaria, inicialmente pensada para acompanhar sopas, conquistou o paladar dos amantes de pão pelo mundo todo. Curiosamente, a receita original não é australiana, mas a adaptação e popularização nos Estados Unidos a tornaram um ícone da culinária internacional. No Brasil.",
+        link : "https://www.google.com/search?q=Pão+australiano&client=opera-gx&hs=e9L&sca_esv=609ef708c28b7711&sxsrf=ADLYWIJd-Gm0dwjzObYdUwOWgZbAAIgsjw%3A1725721215749&ei=f2rcZrq2Lebb1sQPxOXmiAI&ved=0ahUKEwj68a7AjLGIAxXmrZUCHcSyGSEQ4dUDCBA&uact=5&oq=Pão+australiano&gs_lp=Egxnd3Mtd2l6LXNlcnAaAhgBIhBQw6NvIGF1c3RyYWxpYW5vMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMgsQABiABBjLARiLA0ipDlCxCFixCHABeACQAQCYAb8BoAG_AaoBAzAuMbgBA8gBAPgBAfgBApgCAqAC0wGoAg3CAgcQIxgnGOoCwgIUEAAYgAQY4wQYtAIY6QQY6gLYAQGYAw-6BgYIARABGAGSBwMxLjGgB9sF&sclient=gws-wiz-serp",
+        tags : " bom acompanhamento popular pao "
+    },
+    {
+        título : "Pão brioche",
+        descricao : "é um pão que encanta paladares há séculos. Originário da França, acredita-se que seu nome derive da palavra francesa “brioche”, que significa “massa amassada”. Sua receita tradicional leva uma quantidade generosa de manteiga, ovos e açúcar, o que confere ao pão uma riqueza e um sabor inigualáveis. Curiosamente, o brioche era considerado um alimento de luxo na França antiga, sendo consumido principalmente pela nobreza. Hoje em dia, ele é apreciado em todo o mundo e utilizado em diversas preparações, como o tradicional pão de ló, o choux e até mesmo em hambúrgueres gourmet.",
+        link : "https://www.google.com/search?q=pão+brioche+curiosidades&client=opera-gx&sca_esv=609ef708c28b7711&sxsrf=ADLYWIKo2HZjzFGBPblBAl0QXGc6QmBf5A%3A1725721876203&ei=FG3cZuiFDLfT1sQP9sTkyAs&oq=pão+brioche+curio&gs_lp=Egxnd3Mtd2l6LXNlcnAaAhgCIhJww6NvIGJyaW9jaGUgY3VyaW8qAggAMgUQIRigATIFECEYoAFIuytQnQZY5CNwAHgBkAEAmAHFAaABlQiqAQMwLja4AQHIAQD4AQGYAgagArMIwgIIEAAYgAQYywHCAgsQABiABBjLARiLA8ICBhAAGBYYHsICCBAAGIAEGKIEmAMAkgcFMC41LjGgB6EZ&sclient=gws-wiz-serp",
+        tags : " hamburguer fino samduiche pao samduba brioxe "
+    },
+    {
+        título : "Pão de queijo",
+        descricao : "Sua origem exata é um pouco nebulosa, mas acredita-se que tenha surgido da necessidade de aproveitar o excedente de queijo produzido nas fazendas mineiras. A receita original, simples e saborosa, se espalhou por todo o país e ganhou diversas variações ao longo dos anos. Uma curiosidade interessante é que, apesar de ser tão popular, o pão de queijo não possui uma data oficial de criação.",
+        link : "https://pt.wikipedia.org/wiki/Pão_de_queijo",
+        tags : " mineiro pao"
+    },
+    {
+        título : "Bisnaguinha",
+        descricao : "A bisnaguinha, com sua forma alongada e fatia macia, é um dos pães mais populares do Brasil. Sua origem é um pouco incerta, mas acredita-se que tenha surgido no início do século XX, em um contexto de industrialização da panificação. A Panco é frequentemente citada como uma das primeiras empresas a produzir esse tipo de pão em larga escala. A bisnaguinha conquistou o paladar dos brasileiros por sua praticidade e versatilidade, sendo perfeita para o café da manhã, lanches rápidos e até mesmo para acompanhar refeições mais elaboradas. Uma curiosidade interessante é a disputa entre as marcas Panco e Seven Boys sobre quem foi a primeira a produzir a bisnaguinha, tornando-a um verdadeiro ícone da cultura alimentar brasileira.",
+        link : "https://jopioneiro.com/curiosidade-do-dia-panco-ou-seven-boys-quem-criou-as-bisnaguinhas/",
+        tags : "  pao"
+    },
+    {
+        título : "Focaccia",
+        descricao : "A focaccia, com sua crosta levemente crocante e miolo macio e perfumado, é um pão italiano que conquistou paladares em todo o mundo. Originária da Ligúria, na Itália, a focaccia é tradicionalmente preparada com azeite extra virgem, sal e ervas frescas, como alecrim e orégano. Sua história remonta à antiguidade, quando os romanos já preparavam um pão semelhante. Ao longo dos séculos, a focaccia evoluiu e ganhou diversas variações regionais, com adição de ingredientes como cebola, queijo e até mesmo frutas secas. Uma curiosidade interessante é que a focaccia é frequentemente consumida como acompanhamento de sopas e saladas, mas também pode ser recheada e servida como um lanche completo.",
+        link : "https://pt.wikipedia.org/wiki/Focaccia",
+        tags : "  pao"
+    },
+
 ]
